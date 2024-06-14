327520763ecf7155a4866838b59692807303daab
Arya Samik <90042953+AryaSamik@users.noreply.github.com>
2024-06-15 01:48:34 +0530

    Update Alumnii.jsx

--- src/Components/Pages/Alumnii.jsx
+++ src/Components/Pages/Alumnii.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom"
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Alumnii(){
     const location = useLocation();
@@ -7,12 +8,29 @@ export default function Alumnii(){
 
     let navigate = useNavigate();
 
+    let [yearValue, setYearValue] = useState((new Date().getFullYear()).toString());
+    let [year, setYear] = useState(yearValue);
+
+    let handleFilterYearChange = (event) => {
+        setYearValue(event.target.value);
+    }
+
+    let handleYear = (event) => {
+        setYear(yearValue);
+    }
+
     return(
         <div>
            <h2>Alumnii Page</h2>
+            <label htmlFor="filter-year">Year Of Birth</label>
+           <input type="text" id="filter-year" value={yearValue} onChange={handleFilterYearChange} name="year"></input>
+           <br />
+           <button onClick={handleYear}>Done</button>
+           <br />
+           <br />
            <ul>
             {
-                data.filter(d => d.verified===true).map(d => (
+                data.filter(d => (d.verified===true && d.dateofbirth.slice(0,4)===year)).map(d => (
                     <li key={d._id}>
                         Name: {d.firstname+" "+((d.middlename)?d.middlename:"")+" "+d.lastname}
                         <br></br>
